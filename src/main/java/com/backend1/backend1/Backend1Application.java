896ced80ead091ed5a20054a781c23c90e8e4f5d
Joakim Samuelsson <joakim.samuelsson@elplusplus.com>
2026-05-13 11:59:07 +0200

    init

---
+++ src/main/java/com/backend1/backend1/Backend1Application.java
@@ -0,0 +1,13 @@
+package com.backend1.backend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend1Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(Backend1Application.class, args);
+    }
+
+}
