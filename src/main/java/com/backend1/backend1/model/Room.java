727c613517e6f828d3e8320fac6ab3b82daebadb
MDAX1 <amini_pk@hotmail.com>
2026-05-18 22:18:19 +0200

    add Room entity with OneToMany relation to Booking

--- src/main/java/com/backend1/backend1/model/Room.java
+++ src/main/java/com/backend1/backend1/model/Room.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,23 +17,16 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String roomNumber;
 
     @Enumerated(EnumType.STRING)
     private RoomType type;
 
-    private int extraBeds = 0;
+    private int extraBeds;
+
     private BigDecimal pricePerNight;
 
-    public int getCapacity() {
-        if (type == RoomType.SINGLE) return 1;
-        return 2 + extraBeds;
-    }
-
-    public String getTypeDescription() {
-        if (type == RoomType.SINGLE) return "Enkelrum (1 person)";
-        String extra = extraBeds == 0 ? "inga extrasängar"
-                     : extraBeds == 1 ? "1 extrasäng" : "2 extrasängar";
-        return "Dubbelrum, " + extra + " (max " + getCapacity() + " pers.)";
-    }
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY)
+    private List<Booking> bookings = new ArrayList<>();
 }
